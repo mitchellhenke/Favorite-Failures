7fb692b6f771e1cfb5349a70b71c554d90975f3e
cmcrew <courtney.mitchell@live.com>
2012-11-13 20:08:56 -0500

    working on profile photo upload - currently getting EPIPE error

--- models.js
+++ models.js
@@ -8,6 +8,7 @@ module.exports.configureSchema = function(Schema, mongoose) {
       , creativeEmail : {type: String, required: true}
       , creativeOffice : {type: String, required: true}
       , teamMembers : String
+      , creativePhoto : [ImageSchema]
       
       , projectName     : {type: String, required: true}
       , clientName   : {type: String, required: true}
@@ -15,8 +16,16 @@ module.exports.configureSchema = function(Schema, mongoose) {
       , failedBecause      : String
       //, uploads : [uploadSchema]    
     });
+    
+    // Profile Image
+    var ImageSchema = new Schema({
+        filename: String
+        , timestamp : { type: Date, default: Date.now }
+        
+    });
 
     // add schemas to Mongoose
     mongoose.model('Project', Project);
+    mongoose.model('Image', ImageSchema);
 
 };
--- web.js
+++ web.js
@@ -7,6 +7,15 @@ var moment = require('moment');
 var ejs = require('ejs'); //embedded javascript template engine
 var app = module.exports = express.createServer();
 var auth = require('http-auth'); //http authentication module
+var fs = require('fs');
+// YOUR BUCKET NAME
+var myBucket = 'favorite_failure';
+var knox = require('knox');
+var S3Client = knox.createClient({
+      key: process.env.AWS_KEY
+    , secret: process.env.AWS_SECRET
+    , bucket: myBucket
+});
 //------------------------- DATABASE CONFIGURATION -----------------------------//
 app.db = mongoose.connect(process.env.MONGOLAB_URI); //connect to the mongolabs database - local server uses .env file
 
@@ -15,6 +24,7 @@ require('./models').configureSchema(schema, mongoose);
 
 // Define your DB Model variables
 var Project = mongoose.model('Project');
+var Image = mongoose.model('Image');
 //------------------------- END DATABASE CONFIGURATION -----------------------------//
 
 //------------------------SERVER CONFIGURATION--------------------//
@@ -25,12 +35,12 @@ app.configure(function() {
     app.set('view engine', 'ejs'); // ejs is our template engine
     app.set('view options',{layout:true}); // use /views/layout.html to manage your main header/footer wrapping template
     
-    // define the static directory for css, img and js files
-    app.use(express.static(__dirname + '/static'));
-
     app.use(express.cookieParser());//Cookies must be turned on for Sessions
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    
+    // define the static directory for css, img and js files
+    app.use(express.static(__dirname + '/static'));
   
     /**** Turn on some debugging tools ****/
     app.use(express.logger());
@@ -46,7 +56,6 @@ var basic = auth({
 // main page - display form
 app.get('/', function(request, response) {
     basic.apply(request,response, function(username) {
-        //response.send("Welcome to private area");
         
     });
     // render the form
@@ -55,36 +64,106 @@ app.get('/', function(request, response) {
 
 app.post('/', function(request,response) {
     console.log('posting new project');
+    console.log('FILES!!!!!!!!!!!!!!!!!!!!!!');
+    console.log(request.files);
     
-    var projectData = {
-          creativeName : request.body.creativeName
-        , creativeEmail : request.body.creativeEmail
-        , creativeOffice : request.body.creativeOffice
-        , teamMembers : request.body.teamMembers
-        , projectName     : request.body.projectName
-        , clientName   : request.body.clientName
-        , twitterPitch   : request.body.pitch
-        , failedBecause      : request.body.failure
-    }
-    
-    console.log('******************************************************');
-    console.log('body is ' + request.body);
-    console.log(projectData);
-    
-    // create a new blog post
-    var project = new Project(projectData);
-    console.log(project);
-    
-    // save the blog post
-    project.save();
-    console.log('project ' + project.projectName + ' saved');
-    
-    response.redirect('/thanks');
+    // 1) Get file information from submitted form
+        filename = request.files.image.filename; // actual filename of file
+        path = request.files.image.path; //will be put into a temp directory
+        type = request.files.image.type; // image/jpeg or actual mime type
+                
+        // 2) create file name with logged in user id + cleaned up existing file name. function defined below.
+        cleanedFileName = cleanFileName(filename);
+        console.log('*******************cleaned file name***********************');
+        console.log(cleanedFileName);
+       
+        // 3) we first read the file
+        fs.readFile(request.files.image.path, function(err, buf){
+        // 3a) We first need to open and read the file
+            fs.readFile(path, function(err, buf){
+                
+                // 3b) prepare PUT to Amazon S3
+                var req = S3Client.put(cleanedFileName, {
+                  'Content-Length': buf.length
+                , 'Content-Type': type
+                });
+                
+                // 3c) prepare 'response' callback from S3
+                req.on('response', function(res){
+                    console.log('Inside req.on');
+                    console.log(S3Client.bucket);
+                    
+                    if (200 == res.statusCode) {
+                        console.log('Inside 200 == res.statusCode');
+                        // create new Image
+                        var newImage = {
+                            filename : cleanedFileName
+                        };
+                        console.log('new image');
+                        console.log(newImage);
+                         //Create Project Object
+                        var projectData = {
+                                creativeName : request.body.creativeName
+                              , creativeEmail : request.body.creativeEmail
+                              , creativeOffice : request.body.creativeOffice
+                              , teamMembers : request.body.teamMembers
+                              , projectName     : request.body.projectName
+                              , clientName   : request.body.clientName
+                              , twitterPitch   : request.body.pitch
+                              , failedBecause      : request.body.failure
+                              //, creativePhoto : [newImage]
+                        };
+                        
+                        console.log('****************** Project Data ************************************');
+                        console.log(projectData);
+                        
+                        // create a new blog post
+                        var project = new Project(projectData);
+                        console.log(project);
+                        
+                        // save the blog post
+                        project.save();
+                        console.log('project ' + project.projectName + ' saved');
+                        
+                        response.redirect('/thanks');
+                    
+                    } else {
+                    
+                        response.send("an error occurred. unable to upload profile photo");
+                    
+                    }
+                });
+            
+                // 3d) finally send the content of the file and end
+                req.end(buf);
+            });
+     
+        }); 
 });
 
 app.get('/thanks', function(request,response) {
     response.render('thanks.html');
 });
+
+var cleanFileName = function(filename) {
+    
+    // cleans and generates new filename for example userID=abc123 and filename="My Pet Dog.jpg"
+    // will return "abc123_my_pet_dog.jpg"
+    fileParts = filename.split(".");
+    
+    //get the file extension
+    fileExtension = fileParts[fileParts.length-1]; //get last part of file
+    
+    //add time string to make filename a little more random
+    d = new Date();
+    timeStr = d.getTime();
+    
+    //name without extension "My Pet Dog"
+    newFileName = fileParts[0];
+    
+    return newFilename = timeStr + "_" + fileParts[0].toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'_') + "." + fileExtension;
+    
+}
 
 // Make server turn on and listen at defined PORT (or port 3000 if is not defined)
 var port = process.env.PORT || 3000;
